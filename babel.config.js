94ff879c026258c133eebf9136e5928def82e354
Carlos Kenny <carlos.kenny.ca@gmail.com>
2020-08-22 00:33:53 -0300

    configuração incial do babel

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env', //converter o código de JS mais moderno para um Js mais antigo dependendo do ambiente, entender quais os recursos mais atuais e converter para a vesão mais atual do browser
+    '@babel/preset-react' //adionar as funcionalidade do react na conversão.
+  ],
+};
